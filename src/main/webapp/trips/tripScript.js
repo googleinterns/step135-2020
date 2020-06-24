f31d9bf1adde18e82c50f3b8d8a9b23053961def
Chris Elliott <elliott77chris@gmail.com>
2020-06-24 22:17:06 +0000

    Add new script.js for trips folder that redirects to homepage if user is not signed in.

--- src/main/webapp/trips/tripScript.js
+++ src/main/webapp/trips/tripScript.js
@@ -14,17 +14,14 @@
 
 // Triggered upon DOM load.
 $(document).ready(() => {
+  // Redirect to homepage if user is not signed in.
   isSignedIn().then((signInStatus) => {
-    // Display "sign in" or "sign out" button depending on sign in status.
-    if (signInStatus) {
-      displayStartTripDesign();
+    if (!signInStatus) {
+      window.location.replace('/');
     } else {
-      displaySignInPage();
+      addHeader();
     }
-  }).catch((error) => {
-    // If an error occurs, print error to console and do not display button.
-    console.error(error);
-  })
+  });
 });
 
 // Returns Promise with the sign in status in a boolean.
@@ -36,69 +33,6 @@ function isSignedIn() {
       reject(error);
     });
   });
-}
-
-// Create the necessary elements and display the sign in page.
-function displaySignInPage() {
-  // Set the width of the content container.
-  setContentWidth('650px');
-
-  // Add background image to site. (This id is defined in CSS.)
-  document.body.id = 'body-background-image';
-
-  // Get the homepage "registration" block to and add elements.
-  const indexRegistrationBlock = document.getElementById('index-registration-block');
-  indexRegistrationBlock.style.display = 'block';
-
-  // Create h1 element as title for TravIS.
-  const titleElement = document.createElement('h1');
-  titleElement.innerText = 'TravIS: Travel Information System';
-  titleElement.className = 'index-registration-block-child';
-
-  // Create p element to describe TravIS.
-  const textElement = document.createElement('p');
-  textElement.innerText = 
-    'TravIS, your personal Travel Information System, allows you to plan ' +
-    'the optimal trip to wherever you want to visit.';
-  textElement.className = 'index-registration-block-child';
-  textElement.id = 'index-registration-block-p';
-
-  // Create form element to allow sign in.
-  const formElement = document.createElement('form');
-  formElement.className = 'index-registration-block-child';
-  formElement.id = 'sign-in-button-form';
-  formElement.action = '/auth';
-  formElement.method = 'POST';
-
-  // Create input element to allow sign in within the form element.
-  const inputElement = document.createElement('input');
-  inputElement.className = 'btn btn-primary';
-  inputElement.type = 'submit';
-  inputElement.value = 'Sign in';
-
-  // Add input element to the form element.
-  formElement.appendChild(inputElement);
-
-  // Add above elements to the homepage "registration" block.
-  indexRegistrationBlock.appendChild(titleElement);
-  indexRegistrationBlock.appendChild(textElement);
-  indexRegistrationBlock.appendChild(formElement);
-}
-
-// User is signed in, show the start trip homepage.
-function displayStartTripDesign() {
-  // Set the width of the content container.
-  setContentWidth('800px');
-
-  // Remove background image id from body.
-  document.body.removeAttribute('id');
-
-  // Get the homepage "start trip" block to and add elements.
-  const indexStartTripBlock = document.getElementById('index-start-trip-block');
-  indexStartTripBlock.style.display = 'block';
-
-  // Add the site header.
-  addHeader();
 }
 
 // Add the site header.
@@ -152,10 +86,4 @@ function addHeader() {
 
   // Add the header as the first child of the content container.
   contentContainer.prepend(header);
-}
-
-// Set the width of the content container.
-function setContentWidth(width) {
-  const contentContainer = document.getElementById('content');
-  contentContainer.style.width = width;
 }
