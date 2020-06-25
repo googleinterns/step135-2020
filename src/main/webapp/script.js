1c5ceed1c7f0fbf20ed8cb4a5cee12ac9502d6e9
Chris Elliott <elliott77chris@gmail.com>
2020-06-25 22:36:10 +0000

    Move frontend to index.html files, and update script.js file to change display status.

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -15,7 +15,7 @@
 // Triggered upon DOM load.
 $(document).ready(() => {
   isSignedIn().then((signInStatus) => {
-    // Display "sign in" or "sign out" button depending on sign in status.
+    // Display the sign-in page or "start trip" form depending on sign in status.
     if (signInStatus) {
       displayStartTripDesign();
     } else {
@@ -49,40 +49,6 @@ function displaySignInPage() {
   // Get the homepage "registration" block to and add elements.
   const indexRegistrationBlock = document.getElementById('index-registration-block');
   indexRegistrationBlock.style.display = 'block';
-
-  // Create h1 element as title for TravIS.
-  const titleElement = document.createElement('h1');
-  titleElement.innerText = 'TravIS: Travel Information System';
-  titleElement.className = 'index-registration-block-child';
-
-  // Create p element to describe TravIS.
-  const textElement = document.createElement('p');
-  textElement.innerText = 
-    'TravIS, your personal Travel Information System, allows you to plan ' +
-    'the optimal trip to wherever you want to visit.';
-  textElement.className = 'index-registration-block-child';
-  textElement.id = 'index-registration-block-p';
-
-  // Create form element to allow sign in.
-  const formElement = document.createElement('form');
-  formElement.className = 'index-registration-block-child';
-  formElement.id = 'sign-in-button-form';
-  formElement.action = '/auth';
-  formElement.method = 'POST';
-
-  // Create input element to allow sign in within the form element.
-  const inputElement = document.createElement('input');
-  inputElement.className = 'btn btn-primary';
-  inputElement.type = 'submit';
-  inputElement.value = 'Sign in';
-
-  // Add input element to the form element.
-  formElement.appendChild(inputElement);
-
-  // Add above elements to the homepage "registration" block.
-  indexRegistrationBlock.appendChild(titleElement);
-  indexRegistrationBlock.appendChild(textElement);
-  indexRegistrationBlock.appendChild(formElement);
 }
 
 // User is signed in, show the start trip homepage.
@@ -98,59 +64,13 @@ function displayStartTripDesign() {
   indexStartTripBlock.style.display = 'block';
 
   // Add the site header.
-  addHeader();
+  displayHeader();
 }
 
-function addHeader() {
-  // Get the content container (add the header to this component).
-  const contentContainer = document.getElementById('content');
-
-  // Create header for site.
-  const header = document.createElement('div');
-  header.id = 'header';
-
-  // Create h3 element as title for TravIS.
-  const titleElement = document.createElement('h3');
-  titleElement.innerText = 'TravIS';
-  titleElement.className = 'header-child float-left';
-  titleElement.id = 'header-title';
-
-  // Create container element to hold the link below.
-  const aContainerElement = document.createElement('div');
-  aContainerElement.className = 'header-child float-right';
-
-  // Create a element to redirect to the "/trips" page.
-  const aElement = document.createElement('a');
-  aElement.innerText = "Trips";
-  aElement.className = 'btn btn-primary';
-  aElement.href = "/trips/";
-
-  // Add a element to container element.
-  aContainerElement.appendChild(aElement);
-
-  // Create form element to allow sign out.
-  const formElement = document.createElement('form');
-  formElement.className = 'header-child float-right';
-  formElement.id = 'sign-out-button-form';
-  formElement.action = '/auth';
-  formElement.method = 'POST';
-
-  // Create input element to allow sign out within the form element.
-  const inputElement = document.createElement('input');
-  inputElement.className = 'btn btn-primary';
-  inputElement.type = 'submit';
-  inputElement.value = 'Sign out';
-
-  // Add input element to the form element.
-  formElement.append(inputElement);
-
-  // Add above elements to the homepage "start trip" block.
-  header.appendChild(titleElement);
-  header.appendChild(formElement);
-  header.appendChild(aContainerElement);
-
-  // Add the header as the first child of the content container.
-  contentContainer.prepend(header);
+function displayHeader() {
+  // Display header for site.
+  const header = document.getElementById('header');
+  header.style.display = 'block';
 }
 
 // Set the width of the content container.
