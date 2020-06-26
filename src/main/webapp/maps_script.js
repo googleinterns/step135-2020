7124cfe142e616da38f07e50efa2a78f7d4cae98
Eshika Saxena <eshikasaxena@outlook.com>
2020-06-26 00:53:15 +0000

    Fix style and clean up code.

--- src/main/webapp/maps_script.js
+++ src/main/webapp/maps_script.js
@@ -35,7 +35,7 @@ window.initMap = function() {
       streetViewControl: false,
   });
 
-  // // Instantiate a directions service.
+  // Instantiate a directions service.
   directionsService = new google.maps.DirectionsService;
 
   // Create a renderer for directions and bind it to the map.
@@ -43,110 +43,36 @@ window.initMap = function() {
   directionsRenderer.setMap(map);
   directionsRenderer.setPanel(document.getElementById('rightPanel'));
 
+  // Hard coded place IDs - will replace with user input
+  let timesSquareID = 'ChIJmQJIxlVYwokRLgeuocVOGVU';
+  let centralParkID = 'ChIJ4zGFAZpYwokRGUGph3Mf37k';
+  let worldTradeID = 'ChIJy7cGfBlawokR5l2e93hsoEA';
+  let empireStateID = 'ChIJtcaxrqlZwokRfwmmibzPsTU';
+  let hotelID = 'ChIJ68J3tfpYwokR2HaRoBcB4xg';
 
-  // Search for Google's office in Australia.
-  var request = {
-    location: map.getCenter(),
-    radius: '500',
-    query: 'Four Seasons Hotel New York'
-  };
+  let waypts = [{location : {'placeId': worldTradeID}},
+                {location : {'placeId': empireStateID}},
+                {location : {'placeId': timesSquareID}},
+                {location : {'placeId': centralParkID}},
+                ];
 
-  var service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
+  // Create a DirectionsRequest with hotel as start/end and 
+  // POIs as waypoints (stops on the route)
+  directionsService.route({
+    origin: {'placeId': hotelID},
+    destination: {'placeId': hotelID},
+    waypoints: waypts,
+    optimizeWaypoints: true,
+    travelMode: 'DRIVING'
+  }, function(response, status) {
+    // Show directions when found
+    if (status === 'OK') {
+      directionsRenderer.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
 }
-
-// Checks that the PlacesServiceStatus is OK, and adds a marker
-// using the place ID and location from the PlacesService.
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    let timesSquareID = 'ChIJmQJIxlVYwokRLgeuocVOGVU';
-    
-    let centralParkID = 'ChIJ4zGFAZpYwokRGUGph3Mf37k';
-    
-    let worldTradeID = 'ChIJy7cGfBlawokR5l2e93hsoEA';
-    let empireStateID = 'ChIJtcaxrqlZwokRfwmmibzPsTU';
-    let hotelID = 'ChIJ68J3tfpYwokR2HaRoBcB4xg';
-
-
-    let waypts = [{location : {'placeId': worldTradeID}},
-                  {location : {'placeId': empireStateID}},
-                  {location : {'placeId': timesSquareID}},
-                  {location : {'placeId': centralParkID}},
-                  ];
-
-    // Retrieve the start and end locations and create a DirectionsRequest using
-    directionsService.route({
-      // Times square
-      origin: {'placeId': hotelID},
-      destination: {'placeId': hotelID},
-      waypoints: waypts,
-      optimizeWaypoints: true,
-      travelMode: 'DRIVING'
-    }, function(response, status) {
-      // Route the directions and pass the response to a function to create
-      // markers for each step.
-      if (status === 'OK') {
-        directionsRenderer.setDirections(response);
-      } else {
-        window.alert('Directions request failed due to ' + status);
-      }
-    });
-  }
-
-};
 
 // Append the 'script' element to 'head'
 document.head.appendChild(script);
-
-// function initMap() {
-//   // Manhattan coords
-//   const coords = {lat: 40.771, lng: -73.974};
-
-//   // Create map centered on Manhattan
-//   let map = new google.maps.Map(
-//       document.getElementById('routeMap'),
-//       {center: coords, 
-//       zoom: 13,
-//       streetViewControl: false,
-//   });
-
-  // // Instantiate a directions service.
-  // let directionsService = new google.maps.DirectionsService;
-
-  // // Create a renderer for directions and bind it to the map.
-  // let directionsRenderer = new google.maps.DirectionsRenderer({map: map});
-  // directionsRenderer.setPanel(document.getElementById('rightPanel'));
-
-  // // Instantiate an info window to hold step text.
-  // let stepDisplay = new google.maps.InfoWindow;
-
-  // let timesSquareID = 'ChIJmQJIxlVYwokRLgeuocVOGVU';
-  // let centralParkID = 'ChIJ4zGFAZpYwokRGUGph3Mf37k';
-  // let worldTradeID = 'ChIJy7cGfBlawokR5l2e93hsoEA';
-  // let empireStateID = 'ChIJtcaxrqlZwokRfwmmibzPsTU';
-
-  // let waypts = [{location : timesSquareID}, {location : centralParkID},
-  //               {location : worldTradeID}, {location : empireStateID}];
-
-  // let hotelID = 'ChIJ68J3tfpYwokR2HaRoBcB4xg';
-
-  // // Retrieve the start and end locations and create a DirectionsRequest using
-  // // WALKING directions.
-  // directionsService.route({
-  //   // Times square
-  //   origin: {placeID:hotelID},
-  //   destination: {placeID:hotelID},
-  //   waypoints: waypts,
-  //   optimizeWaypoints: true,
-  //   travelMode: 'DRIVING'
-  // }, function(response, status) {
-  //   // Route the directions and pass the response to a function to create
-  //   // markers for each step.
-  //   if (status === 'OK') {
-  //     directionsRenderer.setDirections(response);
-  //   } else {
-  //     window.alert('Directions request failed due to ' + status);
-  //   }
-  // });
-// }
-
