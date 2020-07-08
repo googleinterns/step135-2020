8a48c4e2e5e1a05e9353d9995c1bd4d725c22e3e
Chris Elliott <elliott77chris@gmail.com>
2020-07-08 22:52:41 +0000

    Add initial JavaScript functions to build trip cards.

--- src/main/webapp/trips/tripScript.js
+++ src/main/webapp/trips/tripScript.js
@@ -27,3 +27,61 @@ $(document).ready(() => {
 function setContentWidth(width) {
   document.getElementById('content').style.width = width;
 }
+
+// Fetch the trip details for the user from the database, add trip cards.
+function addTripCards() {
+  fetch('/user-trips').then(response => response.json()).then((userTripObject) => {
+    resolve(userTripObject);
+  }).catch((error) => {
+    reject(error);
+  });
+}
+
+// Build HTML trip card to display.
+function buildTripCard(tripTitle, imageSrc, startDate, endDate, tripId) {
+  // Create the card container.
+  const cardContainer = document.createElement('div');
+  cardContainer.className = 'card';
+  cardContainer.style.width = '18rem';
+
+  // Create a trip image object (image of the destination).
+  const tripImage = document.createElement('img');
+  tripImage.className = 'card-img-top';
+  tripImage.src = imageSrc;
+  tripImage.alt = 'Image of ' + tripTitle;
+
+  // Create a container for the card body.
+  const cardBodyContainer = document.createElement('div');
+  cardBodyContainer.className = 'card-body';
+
+  // Create a title, dates, and action buttons (calendar and maps) for the card.
+  const titleElement = document.createElement('h5');
+  titleElement.className = 'card-title';
+  titleElement.innerText = tripTitle;
+
+  const datesElement = document.createElement('p');
+  datesElement.className = 'card-text';
+  datesElement.innerText = startDate + ' - ' + endDate;
+
+  const calendarButton = document.createElement('a');
+  calendarButton.className = 'btn btn-primary';
+  calendarButton.href = '../calendar.html?tripId=' + tripId;
+  calendarButton.innerText = 'Visit Calendar';
+
+  const mapsButton = document.createElement('a');
+  mapsButton.className = 'btn btn-primary';
+  mapsButton.href = '../maps.html?tripId=' + tripId;
+  mapsButton.innerText = 'Visit Maps';
+
+  // Add the body elements to the card body container.
+  cardBodyContainer.appendChild(titleElement);
+  cardBodyContainer.appendChild(datesElement);
+  cardBodyContainer.appendChild(calendarButton);
+  cardBodyContainer.appendChild(mapsButton);
+
+  // Add the trip image and card body to the card container.
+  cardContainer.appendChild(tripImage);
+  cardContainer.appendChild(cardBodyContainer);
+
+  return cardContainer;
+}
