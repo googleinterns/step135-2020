888a9bdd487ce6768bf2f148435f9d9a0e992bd2
Eshika Saxena <eshikasaxena@outlook.com>
2020-07-08 19:27:09 +0000

    Generate directions request in backend.

--- src/main/java/com/google/sps/servlets/TripServlet.java
+++ src/main/java/com/google/sps/servlets/TripServlet.java
@@ -21,6 +21,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.maps.DirectionsApi.RouteRestriction;
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.errors.NotFoundException;
+import com.google.maps.model.AddressType;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.GeocodedWaypointStatus;
+import com.google.maps.model.LatLng;
+import com.google.maps.model.TrafficModel;
+import com.google.maps.model.TransitMode;
+import com.google.maps.model.TransitRoutingPreference;
+import com.google.maps.model.TravelMode;
+import com.google.maps.model.Unit;
+import com.google.maps.errors.ApiException;
+import com.google.maps.DirectionsApiRequest;
+
 @WebServlet("/calculate-trip")
 public class TripServlet extends HttpServlet {
 
@@ -34,6 +50,34 @@ public class TripServlet extends HttpServlet {
       String paramName = params.nextElement();
       response.getWriter().println(paramName + ": " + request.getParameter(paramName));
     }
+
+   	GeoApiContext distCalcer = new GeoApiContext.Builder()
+		    .apiKey("AIzaSyCmQyeeWI_cV0yvh1SuXYGoLej3g_D9NbY")
+		    .build();
+
+    // try (LocalTestServerContext sc =
+    //     new LocalTestServerContext("{\"routes\": [{}],\"status\": \"OK\"}")) {
+    DirectionsApiRequest directionsRequest = DirectionsApi.newRequest(distCalcer)
+        .origin("Boston,MA")
+        .destination("Concord,MA")
+        .waypoints("Charlestown,MA", "Lexington,MA")
+        .optimizeWaypoints(true)
+        .mode(TravelMode.DRIVING);
+
+    try {
+      DirectionsResult dirResult = directionsRequest.await();
+          
+    } catch (ApiException | InterruptedException e) {
+      throw new IOException(e);
+    } 
+
+    // sc.assertParamValue("Boston,MA", "origin");
+    // sc.assertParamValue("Concord,MA", "destination");
+    // sc.assertParamValue("Charlestown,MA|Lexington,MA", "waypoints");
+    // } catch (Exception e) {
+    //   System.out.println("ERROR");
+    // }
+
   }
 
 }
