cb83b8673f4ae8053ea8a012b49756bc2ffa9619
Adam Weidman <adamfweidman@gmail.com>
2020-07-09 17:08:10 +0000

    fix nits

--- src/main/java/com/google/sps/data/Event.java
+++ src/main/java/com/google/sps/data/Event.java
@@ -17,7 +17,6 @@ package com.google.sps.data;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-
 import java.time.format.DateTimeFormatter;
 
 /**
@@ -46,7 +45,7 @@ public class Event {
    * 
    * @param name name of the location (NOT address)
    * @param address exact address of the POI
-   * @param startTime start of activity (YYYY-MM-DD'T'HH:MM:SS)
+   * @param startTime start of activity
    * @param travelTime time spent traveling to next location (minutes). 
    *        Null if last location of the day.
    * @param timeAtLocation time spent at POI (minutes)
@@ -62,11 +61,11 @@ public class Event {
   }
 
   /**
-   * Constructor that takes in time spent at location
+   * Constructor that assume one hour default spent at location
    * 
    * @param name name of the location (NOT address)
    * @param address exact address of the POI
-   * @param startTime start of activity (YYYY-MM-DD'T'HH:MM:SS)
+   * @param startTime start of activity
    * @param travelTime time spent traveling to next location (minutes). 
    *        Null if last location of the day.
    */
