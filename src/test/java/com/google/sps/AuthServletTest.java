6afedac082e23cc8fba07d10ecf606e8bbb0e243
Chris Elliott <elliott77chris@gmail.com>
2020-07-14 20:39:09 +0000

    Remove unused getParameter(...) function.

---
+++ src/test/java/com/google/sps/AuthServletTest.java
@@ -0,0 +1,60 @@
+
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps;
+
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.sps.data.User;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class AuthServletTest {
+
+  // Add constants for testing User class.
+  public static final String EMAIL = "testemail@gmail.com";
+
+  // Add helper to allow datastore testing in local JUnit tests.
+  // See https://cloud.google.com/appengine/docs/standard/java/tools/localunittesting.
+  private final LocalServiceTestHelper helper =
+      new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+
+  @Before
+  public void setUp() {
+    helper.setUp();
+  }
+
+  @After
+  public void tearDown() {
+    helper.tearDown();
+  }
+  
+  @Test
+  public void testUserBuildEntity() {
+    User user = new User(EMAIL);
+    Entity userEntity = user.buildEntity();
+
+    // Confirm email variable.
+    Assert.assertEquals(EMAIL, userEntity.getProperty(User.USER_EMAIL));
+  }
+}
