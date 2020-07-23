fb67036b23272e47c64fcfa8d291f24f09272819
Adam Weidman <adamfweidman@gmail.com>
2020-07-23 22:55:54 +0000

    changes from EventPopUp

--- src/main/webapp/calendar-script.js
+++ src/main/webapp/calendar-script.js
@@ -12,8 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+let script = document.createElement('script');
+script.src = 'https://maps.googleapis.com/maps/api/js?key=' + config.API_KEY + '&libraries=geometry,places&callback=initMod';
+// 'https://maps.googleapis.com/maps/api/js?key=' + config.API_KEY + 
+  //'&libraries=&callback=initMod';
+script.defer = true;
+script.async = true;
+
+var map;
+
 // Triggered upon DOM load.
-$(document).ready(() => {
+window.initMod = function() {
   var calendarEl = document.getElementById('calendar');
   var calendar = new FullCalendar.Calendar(calendarEl, {
     headerToolbar: {
@@ -24,10 +33,24 @@ $(document).ready(() => {
     initialView: 'timeGridWeek',
     navLinks: true,
     dayMaxEvents: true, //alow "more" link when too many events on one day
+    eventClick: function(info) {
+      var eventObj = info.event;
+
+      // title of pop-up
+      const modalLabel = document.getElementById('exampleModalLabel');
+      modalLabel.innerHTML = eventObj.title;
+
+      // body of pop-up
+      $('#exampleModalBody').empty();
+      const modalBody = document.getElementById('exampleModalBody');
+
+      createMap(modalBody, eventObj);
+      $('#exampleModal').modal('show');
+    }
   });
   getEvents(calendar);
   calendar.render();
-});
+};
 
 /**
  * retrives the events from /calculate-trip url and dynamically adds the events
@@ -43,8 +66,81 @@ function getEvents(calendar) {
         title: event.name,
         start: event.strStartTime,
         end: event.strEndTime,
-        allDay: false
+        allDay: false,
+        extendedProps: {
+          address: event.address,
+          openTime: '9AM',
+          closeTime: '5PM',
+        }
       });
     });
   });
 }
+
+/**
+ * function that initializes map for popup, w specific address as marker
+ */
+function createMap(modalBody, eventObj) {
+  var geocoder = new google.maps.Geocoder();
+  var coords;
+
+  // text to dispaly in popover
+  let infoDisplay = document.createElement('p');
+  infoDisplay.innerHTML = '<b>Address: </b>' + eventObj.extendedProps.address + '<br>' +
+  '<b>Opening hours: </b>' + eventObj.extendedProps.openTime + '<br>' +
+  '<b>Closing hours: </b>' + eventObj.extendedProps.closeTime + '<br>';
+  modalBody.appendChild(infoDisplay)
+
+  // create new div to hold map
+  const mapDis = document.createElement('div');
+  mapDis.id = 'map'
+  modalBody.appendChild(mapDis);
+
+  geocoder.geocode({ placeId: eventObj.extendedProps.address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      var latitude = results[0].geometry.location.lat();
+      var longitude = results[0].geometry.location.lng();
+      coords = {lat: latitude, lng: longitude};
+    }
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: coords
+    });
+    var marker = new google.maps.Marker({
+      position: coords,
+      map: map,
+      title: eventObj.title
+    });
+  });
+}
+
+function createMap2(modalBody, eventObj) {
+
+  let infoDisplay = document.createElement('p');
+  infoDisplay.innerHTML = '<b>Address: </b>' + eventObj.extendedProps.address + '<br>' +
+  '<b>Opening hours: </b>' + eventObj.extendedProps.openTime + '<br>' +
+  '<b>Closing hours: </b>' + eventObj.extendedProps.closeTime + '<br>';
+  modalBody.appendChild(infoDisplay)
+
+  const mapDis = document.createElement('div');
+  mapDis.id = 'map'
+  modalBody.appendChild(mapDis);
+
+  // The location of Uluru
+  var uluru = {lat: 40.7484, lng: -73.9857};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      center: uluru
+  });
+
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    title: 'Title'
+  });
+}
+
+// Append the 'script' element to the document head.
+document.head.appendChild(script);
