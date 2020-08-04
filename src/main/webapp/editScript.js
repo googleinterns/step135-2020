7fd0e3da0f359eb1d7c4e3797e547ce217ba9d69
Chris Elliott <elliott77chris@gmail.com>
2020-08-04 17:25:18 +0000

    Add edit object content to editScript.js.

--- src/main/webapp/editScript.js
+++ src/main/webapp/editScript.js
@@ -21,8 +21,205 @@ script.async = true;
 
 // Attach callback function to the 'window' object.
 window.initEditScript = () => {
+  // Populate the edit page with content.
+  populateEditPage();
+
   addInputPoiLocationAutofill();
   autoscaleTextInputWidth();
+}
+
+function populateEditPage() {
+  let editObjectContent = {
+    "trip":{
+        "tripName":"My Favorite Trip",
+        "destinationName":"Hotel Seattle",
+        "tripKey":"aglub19hcHBfaWRyIgsSBHVzZXIYgICAgICAkAoMCxIEdHJpcBiAgICAgIDwCQw",
+        "imageSrc":"https://maps.googleapis.com/maps/api/place/photo?maxwidth\u003d400\u0026photoreference\u003dCmRaAAAA_xIcyHJDhBA73aeywi0a2fK5R9BgDPLkH-DVNED6lMjEDsfehkyME3tlU5EgPc5QGQ8DZOTKebRtAIPDiHLi4NqkkHV-JKJ3TYibe4e_UduH_nlI2TfK5jvWeO34op7fEhAxDp_H3_noI6rIgEBcUhvJGhTzVxhoeC264Eh8YZWN6whQp7jRTw\u0026key\u003dAIzaSyCmQyeeWI_cV0yvh1SuXYGoLej3g_D9NbY",
+        "startDate":{
+          "year":2020,
+          "month":8,
+          "day":6
+        },
+        "endDate":{
+          "year":2020,
+          "month":8,
+          "day":6
+        },
+        "numDays":1
+    },
+    "dateEventMap":{
+        "Thursday, 8/6/2020":[
+          {
+              "name":"Space Needle",
+              "address":"Space Needle",
+              "startTime":{
+                "date":{
+                    "year":2020,
+                    "month":8,
+                    "day":6
+                },
+                "time":{
+                    "hour":10,
+                    "minute":7,
+                    "second":0,
+                    "nano":0
+                }
+              },
+              "endTime":{
+                "date":{
+                    "year":2020,
+                    "month":8,
+                    "day":6
+                },
+                "time":{
+                    "hour":11,
+                    "minute":7,
+                    "second":0,
+                    "nano":0
+                }
+              },
+              "strStartTime":"2020-08-06T10:07:00",
+              "strEndTime":"2020-08-06T11:07:00",
+              "travelTime":10
+          },
+          {
+              "name":"Gas Works Park",
+              "address":"Gas Works Park",
+              "startTime":{
+                "date":{
+                    "year":2020,
+                    "month":8,
+                    "day":6
+                },
+                "time":{
+                    "hour":11,
+                    "minute":17,
+                    "second":0,
+                    "nano":0
+                }
+              },
+              "endTime":{
+                "date":{
+                    "year":2020,
+                    "month":8,
+                    "day":6
+                },
+                "time":{
+                    "hour":12,
+                    "minute":17,
+                    "second":0,
+                    "nano":0
+                }
+              },
+              "strStartTime":"2020-08-06T11:17:00",
+              "strEndTime":"2020-08-06T12:17:00",
+              "travelTime":9
+          },
+          {
+              "name":"Woodland Park Zoo",
+              "address":"Woodland Park Zoo",
+              "startTime":{
+                "date":{
+                    "year":2020,
+                    "month":8,
+                    "day":6
+                },
+                "time":{
+                    "hour":12,
+                    "minute":26,
+                    "second":0,
+                    "nano":0
+                }
+              },
+              "endTime":{
+                "date":{
+                    "year":2020,
+                    "month":8,
+                    "day":6
+                },
+                "time":{
+                    "hour":13,
+                    "minute":26,
+                    "second":0,
+                    "nano":0
+                }
+              },
+              "strStartTime":"2020-08-06T12:26:00",
+              "strEndTime":"2020-08-06T13:26:00",
+              "travelTime":3
+          },
+          {
+              "name":"Aurora Bridge",
+              "address":"Aurora Bridge",
+              "startTime":{
+                "date":{
+                    "year":2020,
+                    "month":8,
+                    "day":6
+                },
+                "time":{
+                    "hour":13,
+                    "minute":29,
+                    "second":0,
+                    "nano":0
+                }
+              },
+              "endTime":{
+                "date":{
+                    "year":2020,
+                    "month":8,
+                    "day":6
+                },
+                "time":{
+                    "hour":14,
+                    "minute":29,
+                    "second":0,
+                    "nano":0
+                }
+              },
+              "strStartTime":"2020-08-06T13:29:00",
+              "strEndTime":"2020-08-06T14:29:00",
+              "travelTime":38
+          },
+          {
+              "name":"Snoqualmie Falls",
+              "address":"Snoqualmie Falls",
+              "startTime":{
+                "date":{
+                    "year":2020,
+                    "month":8,
+                    "day":6
+                },
+                "time":{
+                    "hour":15,
+                    "minute":7,
+                    "second":0,
+                    "nano":0
+                }
+              },
+              "endTime":{
+                "date":{
+                    "year":2020,
+                    "month":8,
+                    "day":6
+                },
+                "time":{
+                    "hour":16,
+                    "minute":7,
+                    "second":0,
+                    "nano":0
+                }
+              },
+              "strStartTime":"2020-08-06T15:07:00",
+              "strEndTime":"2020-08-06T16:07:00",
+              "travelTime":33
+          }
+        ]
+    }
+  };
+
+  console.log(editObjectContent);
+  console.log(editObjectContent.trip);
 }
 
 // Add text input POI Google Places autofill.
