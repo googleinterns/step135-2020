7ae3fc5a74f6f8af650ce7afe8a79a43f5605811
Adam Weidman <adamfweidman@gmail.com>
2020-08-06 17:57:03 +0000

    fix indents

--- src/test/java/com/google/sps/TupleTest.java
+++ src/test/java/com/google/sps/TupleTest.java
@@ -43,55 +43,55 @@ public final class TupleTest {
     path.add(4);
     path.add(3);
     path.add(7);
-
+ 
     Tuple tuple = new Tuple(CURR_ANS, path);
     Assert.assertEquals(tuple.getCurrAns(), CURR_ANS);
     Assert.assertEquals(tuple.getCurrPath(), path);
   }
-
+ 
   // curr Ans cannot be less than 0
   @Test(expected = IllegalArgumentException.class)
   public void testConstructorAnsBelowZero() {
     Tuple tuple = new Tuple(-1, new ArrayList<>());
   }
-
+ 
   // curr Ans cannot be less than 0
   @Test(expected = IllegalArgumentException.class)
   public void testConstructorNull() {
     Tuple tuple = new Tuple(CURR_ANS, null);
   }
-
+ 
   // test increment answer
   @Test
   public void testIncrementCurrAns() {
     int numToAdd = 2;
     Tuple tuple = new Tuple(CURR_ANS, new ArrayList<>());
     tuple.incCurrAns(numToAdd);
-
+ 
     Assert.assertEquals(tuple.getCurrAns(), CURR_ANS + numToAdd);
   }
-
+ 
   // error when trying to decrease curr Ans
   @Test(expected = IllegalArgumentException.class)
   public void testCannotDecrAns() {
     Tuple tuple = new Tuple(CURR_ANS, new ArrayList<>());
     tuple.incCurrAns(-1);
   }
-  
+
   // test poi added not below 0
   @Test(expected = IllegalArgumentException.class)
   public void testAddPoiBelowZero() {
     Tuple tuple = new Tuple(CURR_ANS, new ArrayList<>());
     tuple.addIntPoiToPath(-1, TOTAL_POIS);
   }
-
+ 
   // test poi added not greater than total pois
   @Test(expected = IllegalArgumentException.class)
   public void testAddPoiGreaterThanTotal() {
     Tuple tuple = new Tuple(CURR_ANS, new ArrayList<>());
     tuple.addIntPoiToPath(6, TOTAL_POIS);
   }
-
+ 
   // test add pois to path
   @Test
   public void testAddPoisToPath() {
@@ -99,37 +99,37 @@ public final class TupleTest {
     List<Integer> expected = new ArrayList<>();
     expected.add(1);
     expected.add(2);
-  
+ 
     tuple.addIntPoiToPath(1, TOTAL_POIS);
     tuple.addIntPoiToPath(2, TOTAL_POIS);
-
+ 
     Assert.assertEquals(tuple.getCurrPath(), expected);
   }
-
+ 
   @Test
   public void testEqualsSame() {
     Tuple tuple = new Tuple(CURR_ANS, new ArrayList<>());
-
+ 
     Assert.assertTrue(tuple.areEqual(tuple));
   }
-
+ 
   @Test
   public void testEqualsDifEquals() {
     Tuple tuple1 = new Tuple(CURR_ANS, new ArrayList<>());
     tuple1.addIntPoiToPath(1, TOTAL_POIS);
     Tuple tuple2 = new Tuple(CURR_ANS, new ArrayList<>());
     tuple2.addIntPoiToPath(1, TOTAL_POIS);
-
+ 
     Assert.assertTrue(tuple1.areEqual(tuple2));
   }
-
+ 
   @Test
   public void testEqualsFalse() {
     Tuple tuple1 = new Tuple(CURR_ANS, new ArrayList<>());
     tuple1.addIntPoiToPath(1, TOTAL_POIS);
     Tuple tuple2 = new Tuple(CURR_ANS, new ArrayList<>());
     tuple2.addIntPoiToPath(2, TOTAL_POIS);
-
+ 
     Assert.assertFalse(tuple1.areEqual(tuple2));
   }
 }
